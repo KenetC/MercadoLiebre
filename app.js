bfd2b801eda72e62f4975db9c922b79981dc0a99
KenetC <kenetchape@gmail.com>
2021-11-15 10:52:06 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, ()=>{
+    console.log('Servidor funcionando estilos');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
